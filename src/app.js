d7de681ae6f56e718403455bf45e8033f324e701
aaguirre <alejandra36a@gmail.com>
2024-07-11 18:23:35 +0000

    final

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+let dominios =[ ".com", ".es"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for (let i= 0; i< pronoun.length; i++ ){
+  for (let a = 0; a < adj.length; a++){
+    for (let b=0; b< noun.length; b++){
+      for (let c=0; c< dominios.length; c++){
+        console.log(pronoun[i]+adj[a]+ noun[b]+ dominios[c]);
+        
+      }
+    }
+  }
+}
+
