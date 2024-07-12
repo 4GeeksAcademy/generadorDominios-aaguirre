505ea025a2bf4534159b9ce89bedb251c9bd46bb
aaguirre <alejandra36a@gmail.com>
2024-07-12 07:49:05 +0000

    entrega trabajo

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ for (let i= 0; i< pronoun.length; i++ ){
   for (let a = 0; a < adj.length; a++){
     for (let b=0; b< noun.length; b++){
       for (let c=0; c< dominios.length; c++){
-        console.log(pronoun[i]+adj[a]+ noun[b]+ dominios[c]);
+        console.log (pronoun[i]+adj[a]+ noun[b]+ dominios[c]);
         
       }
     }
